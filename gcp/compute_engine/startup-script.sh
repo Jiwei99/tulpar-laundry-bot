1c05607dbb4bdc45e3566fb934ae6021680247a8
Ji Wei <69960220+Jiwei99@users.noreply.github.com>
2023-12-10 16:38:40 +0800

    Update startup-script.sh

--- gcp/compute_engine/startup-script.sh
+++ gcp/compute_engine/startup-script.sh
@@ -1,6 +1,6 @@
 # Install or update needed software
 sudo apt-get update
-sudo apt-get install -yq git supervisor python3 python3-pip python3-distutils
+sudo apt-get install -yq git supervisor python3 python3-pip python3-distutils tmux
 
 # Fetch source code
 ## To be continued
